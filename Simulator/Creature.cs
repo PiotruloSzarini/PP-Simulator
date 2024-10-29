e01e51fec8b57916643977da3fb8f0073d35b17f
PiotruloSzarini <piotr.szar@vp.pl>
2024-10-29 20:57:08 +0100

    Lab-3

--- Simulator/Creature.cs
+++ Simulator/Creature.cs
@@ -1,4 +1,4 @@
-﻿namespace Simulator;
+namespace Simulator;
 internal class Creature
 {
     private int level = 1;
@@ -43,5 +43,21 @@ internal class Creature
 
     public void SayHi() => Console.WriteLine($"Hi, I'm {Name}, my level is {Level}.");
     public int Upgrade() => level < 10 ? ++level : level;
+    public void Go(Direction direction)
+    {
+        Console.WriteLine($"{name} goes {direction.ToString().ToLower()}.");
+    }
+    public void Go(Direction[] directions)
+    {
+        foreach (var direction in directions)
+        {
+            Go(direction);
+        }
+    }
+    public void Go(string directions)
+    {
+        var parsedDirections = DirectionParser.Parse(directions);
+        Go(parsedDirections);
+    }
     public string Info => $"{Name} [{Level}]";
 }
--- Simulator/obj/Debug/net8.0/Simulator.AssemblyInfo.cs
+++ Simulator/obj/Debug/net8.0/Simulator.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Simulator")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dbab281924749a7ab661c60f7b38e0bd38f47514")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+418bfd2a0f1673e3349e0a2604d5f1054dd8415b")]
 [assembly: System.Reflection.AssemblyProductAttribute("Simulator")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Simulator")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
