c9c020c50fc1f6ce8f6943c637e5d68d301892f2
PiotruloSzarini <piotr.szar@vp.pl>
2024-11-26 20:08:15 +0100

    SmallMap

--- Simulator/DirectionParser.cs
+++ Simulator/DirectionParser.cs
@@ -1,7 +1,7 @@
 ﻿namespace Simulator;
 public static class DirectionParser
 {
-    public static Direction[] Parse(string input)
+   public static List<Direction> Parse(string input)
     {
         var directions = new List<Direction>();
         foreach (var ch in input.ToUpper())
@@ -23,6 +23,6 @@ public static class DirectionParser
                 directions.Add(Direction.Left);
             }
         }
-        return directions.ToArray();
+        return directions;
     }
 }
